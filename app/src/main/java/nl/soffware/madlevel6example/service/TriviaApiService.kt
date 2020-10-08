285605d47b394995c0febe880abfdb57c9b66b50
David Soff <david@soff.nl>
2020-10-08 17:37:22 +0200

    finish example

---
+++ app/src/main/java/nl/soffware/madlevel6example/service/TriviaApiService.kt
@@ -0,0 +1,11 @@
+package nl.soffware.madlevel6example.service
+
+import nl.soffware.madlevel6example.model.Trivia
+import retrofit2.http.GET
+
+interface TriviaApiService {
+
+    // The GET method needed to retrieve a random number trivia.
+    @GET("/random/trivia?json")
+    suspend fun getRandomNumberTrivia(): Trivia
+}
